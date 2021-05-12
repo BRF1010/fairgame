f8abff6b3712d4985d72570e5cd5afd39dcabd48
BRF1010 <82842099+BRF1010@users.noreply.github.com>
2021-05-12 06:12:50 -0600

    Update _Amazon_aio.bat

--- _Amazon_aio.bat
+++ _Amazon_aio.bat
@@ -1,2 +1,2 @@
 REM This code is work in progress. Do not ask for support in Discord running this code.
-cmd /k pipenv run python app.py amazon-aio --delay=3
+cmd /k pipenv run python app.py amazon-aio --delay=2
