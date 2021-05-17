1588271e573a682528096b5b7302ab2c413766f0
BRF1010 <82842099+BRF1010@users.noreply.github.com>
2021-05-17 13:46:49 -0600

    cut out html 200 saves

--- stores/amazon_monitoring.py
+++ stores/amazon_monitoring.py
@@ -224,8 +224,9 @@ class AmazonMonitor(aiohttp.ClientSession):
         delay = self.delay
         end_time = time.time() + delay
         status, response_text = await self.aio_get(url=self.item.furl.url)
-
-        save_html_response(f"{self.item.id}_stock-check", status, response_text)
+        #Cut out html 200 page saves
+        if (status != 200):
+            save_html_response(f"{self.item.id}_stock-check", status, response_text)
 
         # do this after each request
         fail_counter = check_fail(status=status, fail_counter=fail_counter)
@@ -288,7 +289,9 @@ class AmazonMonitor(aiohttp.ClientSession):
             end_time = time.time() + delay
             #log.debug(f"AMAZON URL {self.item.furl.url}")
             status, response_text = await self.aio_get(url=self.item.furl.url)
-            save_html_response(f"{self.item.id}_stock-check", status, response_text)
+            #cut out html 200 saves
+            if (status != 200):
+                save_html_response(f"{self.item.id}_stock-check", status, response_text)
             
             log.debug(f"ASIN {self.item.id} returned HTML status {status} using proxy {self.connector.proxy_url}")
 
